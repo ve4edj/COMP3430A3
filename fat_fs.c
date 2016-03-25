327c3a27f3dfc4b40697b83b67c9f7ed61d1f513
Erik Johnson <erik@johnson.ac>
2016-03-25 16:44:07 -0500

    added c file

---
+++ fat_fs.c
@@ -0,0 +1,41 @@
+#include "fat_fs.h"
+
+FS_Instance *fs_create_instance(char *image_path) {
+
+}
+
+FS_CurrentDir fs_get_root(FS_Instance *fat_fs) {
+
+}
+
+void print_info(FS_Instance *fat_fs) {
+
+}
+
+void print_dir(FS_Instance *fat_fs, FS_CurrentDir current_dir) {
+
+}
+
+FS_CurrentDir change_dir(FS_Instance *fat_fs, FS_CurrentDir current_dir, char *path) {
+
+}
+
+void get_file(FS_Instance *fat_fs, FS_CurrentDir current_dir, char *path, char *local_path) {
+
+}
+
+void put_file(FS_Instance *fat_fs, FS_CurrentDir current_dir, char *path, char *local_path) {
+
+}
+
+void make_dir(FS_Instance *fat_fs, FS_CurrentDir current_dir, char *path) {
+
+}
+
+void delete_file(FS_Instance *fat_fs, FS_CurrentDir current_dir, char *path) {
+
+}
+
+void fs_cleanup(FS_Instance *fat_fs) {
+
+}

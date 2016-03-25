6a355f117bf643b41107c30224c8ec242aa6eef1
Erik Johnson <erik@johnson.ac>
2016-03-25 16:23:04 -0500

    added provided files

---
+++ fat.h
@@ -0,0 +1,98 @@
+#ifndef FAT12_H
+#define FAT12_H
+
+#include <inttypes.h>
+
+/* All values as stored on disk are little-endian */
+
+/* boot sector constants */
+#define BS_OEMName_LENGTH 8
+#define BS_VolLab_LENGTH 11
+#define BS_FilSysType_LENGTH 8 
+
+/* directory entry constants */
+#define DIR_Name_LENGTH 11
+
+#pragma pack(push)
+#pragma pack(1)
+struct fatBS_struct {
+	uint8_t BS_jmpBoot[3];
+	uint8_t BS_OEMName[BS_OEMName_LENGTH];
+	uint16_t BPB_BytsPerSec;
+	uint8_t BPB_SecPerClus;
+	uint16_t BPB_RsvdSecCnt;
+	uint8_t BPB_NumFATs;
+	uint16_t BPB_RootEntCnt;
+	uint16_t BPB_TotSec16;
+	uint8_t BPB_Media;
+	uint16_t BPB_FATSz16;
+	uint16_t BPB_SecPerTrk;
+	uint16_t BPB_NumHeads;
+	uint32_t BPB_HiddSec;
+	uint32_t BPB_TotSec32;
+	// FAT12/16:
+	uint8_t BPB_DrvNum;
+	uint8_t BPB_Reserved1;
+	uint8_t BPB_BootSig;
+	uint32_t BPB_VolID;
+	uint8_t BS_VolLab[BS_VolLab_LENGTH];
+	uint8_t BS_FilSysType[BS_FilSysType_LENGTH];
+	uint8_t BS_CodeReserved[448];
+	uint8_t BS_SigA;
+	uint8_t BS_SigB;
+	// FAT32:
+	/*
+	uint32_t BPB_FATSz32;
+	uint16_t BPB_ExtFlags;
+	uint8_t BPB_FSVerLow;
+	uint8_t BPB_FSVerHigh;
+	uint32_t BPB_RootClus;
+	uint16_t BPB_FSInfo;
+	uint16_t BPB_BkBootSec;
+	char BPB_reserved[12];
+	uint8_t BS_DrvNum;
+	uint8_t BS_Reserved1;
+	uint8_t BS_BootSig;
+	uint32_t BS_VolID;
+	char BS_VolLab[BS_VolLab_LENGTH];
+	char BS_FilSysType[BS_FilSysType_LENGTH];
+	char BS_CodeReserved[420];
+	uint8_t BS_SigA;
+	uint8_t BS_SigB;
+	*/
+};
+
+struct fatDate_struct {
+		uint16_t day	 : 5;
+		uint16_t month : 4;
+		uint16_t year	 : 7;
+};
+
+struct fatTime_struct {
+		uint16_t sec : 5;
+		uint16_t min : 6;
+		uint16_t hour: 5;
+};
+
+struct fatEntry_struct {
+		uint8_t DIR_Name[DIR_Name_LENGTH];
+		uint8_t DIR_Attr;
+		uint8_t DIR_NTRes;
+		uint8_t DIR_CrtTimeTenth;
+		struct fatTime_struct DIR_CrtTime;
+		struct fatDate_struct DIR_CrtDate;
+		struct fatDate_struct DIR_LstAccDate;
+		uint16_t DIR_FstClusHI;
+		struct fatTime_struct DIR_WrtTime;
+		struct fatDate_struct DIR_WrtDate;
+		uint16_t DIR_FstClusLO;
+		uint32_t DIR_FileSize;
+};
+#pragma pack(pop)
+
+typedef struct fatBS_struct fatBS;
+typedef struct fatDate_struct fatDate;
+typedef struct fatTime_struct fatTime;
+typedef struct fatEntry_struct fatEntry;
+
+#endif
---
+++ shell.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include "fat_fs.h"
+
+#define BUF_SIZE 256
+#define CMD_INFO "INFO"
+#define CMD_DIR "DIR"
+#define CMD_CD "CD"
+#define CMD_GET "GET"
+
+#define CMD_PUT "PUT"
+#define CMD_MD "MD"
+#define CMD_DEL "DEL"
+
+int main(int argc, char *argv[]) {
+	int done = 0, valid_cmd;
+	FS_Instance *fat_fs;
+	FS_CurrentDir current_dir;
+	char buffer[BUF_SIZE];
+	char buffer_raw[BUF_SIZE];
+	char *arg1, *arg2;
+
+	if (2 != argc) {
+		fprintf(stderr, "Usage: %s fat32image\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	fat_fs = fs_create_instance(argv[1]);
+	if (NULL == fat_fs) {
+		fprintf(stderr, "Invalid FAT12 image %s.\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	
+	current_dir = fs_get_root(fat_fs);
+
+	while (!done) {
+		printf("> ");
+
+		if (NULL == fgets(buffer_raw, BUF_SIZE, stdin)) {
+			done = 1;
+
+		} else {
+			valid_cmd = 1;
+
+			buffer_raw[strlen(buffer_raw)-1] = '\0'; /* cut new line */
+			for (int i=0; i < strlen(buffer_raw)+1; i++)
+				buffer[i] = toupper(buffer_raw[i]);
+			arg1 = strchr(buffer, ' ');
+	
+			if (strncmp(buffer, CMD_INFO, strlen(CMD_INFO)) == 0)
+				print_info(fat_fs); 
+
+			else if (strncmp(buffer, CMD_DIR, strlen(CMD_DIR)) == 0)
+				print_dir(fat_fs, current_dir); 
+	
+			else if (NULL != arg1) {
+				arg2 = strchr(arg1, ' ');
+
+				if (strncmp(buffer, CMD_CD, strlen(CMD_CD)) == 0)
+						current_dir = change_dir(fat_fs, current_dir, arg1+1);
+
+				// else if (strncmp(buffer, CMD_MD, strlen(CMD_MD)) == 0)
+				//	 current_dir = make_dir(fat_fs, current_dir, arg1+1);
+
+				// else if (strncmp(buffer, CMD_DEL, strlen(CMD_DEL)) == 0)
+				//	 current_dir = delete_file(fat_fs, current_dir, arg1+1);
+
+				else if (NULL != arg2) {
+					if (strncmp(buffer, CMD_GET, strlen(CMD_GET)) == 0)
+						get_file(fat_fs, current_dir, arg1+1, arg2+1);
+
+					// else if (strncmp(buffer, CMD_PUT, strlen(CMD_PUT)) == 0)
+					//	 save_file(fat_fs, current_dir, arg1+1, arg2+1);
+
+					else
+						valid_cmd = 0;
+				} else
+					valid_cmd = 0;
+			} else 
+				valid_cmd = 0;
+			
+			if (!valid_cmd && '\0' != buffer[0]) {
+				printf("\nUnknown command %s.\n", buffer);
+			}
+		}
+	}
+	printf("\nExiting cleanly.\n");
+	
+	fs_cleanup(fat_fs);
+	
+	return EXIT_SUCCESS;
+}

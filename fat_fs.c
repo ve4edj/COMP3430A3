0f5b2064aae3531ecb8a8e30512f45f080829009
Erik Johnson <erik@johnson.ac>
2016-04-02 21:08:35 -0500

    root dir retreival and helper functions

--- fat_fs.c
+++ fat_fs.c
@@ -63,8 +63,35 @@ FS_Instance * fs_create_instance(char * image_path) {
 	return fsi;
 }
 
-FS_CurrentDir fs_get_root(FS_Instance * fat_fs) {
+uint64_t calcFATOffset(uint64_t cluster, FS_Instance * fsi) {
+	switch (fsi->type) {
+		case FS_FAT12:
+			return cluster + (cluster / 2);
+		case FS_FAT16:
+			return cluster * 2;
+		case FS_FAT32:
+			return cluster * 4;
+	}
+}
 
+uint32_t getFATSectorNum(uint64_t cluster, FS_Instance * fsi) {
+	return (fsi->bootsect->BPB_RsvdSecCnt + (calcFATOffset(cluster, fsi) / fsi->bootsect->BPB_BytsPerSec));
+}
+
+uint32_t getFATEntryOffset(uint64_t cluster, FS_Instance * fsi) {
+	return (calcFATOffset(cluster, fsi) % fsi->bootsect->BPB_BytsPerSec);
+}
+
+FS_CurrentDir fs_get_root(FS_Instance * fsi) {
+	switch (fsi->type) {
+		case FS_FAT12:
+		case FS_FAT16:
+			return (fsi->bootsect->BPB_RsvdSecCnt + (fsi->bootsect->BPB_NumFATs * fsi->FATsz));
+			break;
+		case FS_FAT32:
+			return fsi->bootsect32->BPB_RootClus;
+			break;
+	}
 }
 
 void loopPrintChar(uint8_t * str, int len) {
@@ -73,94 +100,94 @@ void loopPrintChar(uint8_t * str, int len) {
 	}
 }
 
-void print_info(FS_Instance * fat_fs) {
-	fatBS * bs = fat_fs->bootsect;
+void print_info(FS_Instance * fsi) {
+	fatBS * bs = fsi->bootsect;
 	printf("\n");
 	printf("Disk information:\n-----------------\n");
 	printf("OEM Name: ");
 	loopPrintChar(&(bs->BS_OEMName[0]), BS_OEMName_LENGTH);
 	printf("\n");
 	printf("Volume Label: ");
-	if (fat_fs->type == FS_FAT32)
-		loopPrintChar(&(fat_fs->bootsect32->BS_VolLab[0]), BS_VolLab_LENGTH);
+	if (fsi->type == FS_FAT32)
+		loopPrintChar(&(fsi->bootsect32->BS_VolLab[0]), BS_VolLab_LENGTH);
 	else
-		loopPrintChar(&(fat_fs->bootsect16->BS_VolLab[0]), BS_VolLab_LENGTH);
+		loopPrintChar(&(fsi->bootsect16->BS_VolLab[0]), BS_VolLab_LENGTH);
 	printf("\n");
 	printf("File System Type (read): ");
-	if (fat_fs->type == FS_FAT32)
-		loopPrintChar(&(fat_fs->bootsect32->BS_FilSysType[0]), BS_FilSysType_LENGTH);
+	if (fsi->type == FS_FAT32)
+		loopPrintChar(&(fsi->bootsect32->BS_FilSysType[0]), BS_FilSysType_LENGTH);
 	else
-		loopPrintChar(&(fat_fs->bootsect16->BS_FilSysType[0]), BS_FilSysType_LENGTH);
+		loopPrintChar(&(fsi->bootsect16->BS_FilSysType[0]), BS_FilSysType_LENGTH);
 	printf("\n");
 	printf("Media Type: 0x%2X (%sremovable)\n", bs->BPB_Media, (bs->BPB_Media == 0xF8) ? "non-" : "");
 	const char * units[] = {"B", "kB", "MB", "GB", "TB"};
 	int currUnit = 0;
-	long double scaledSz = fat_fs->totalSize;
+	long double scaledSz = fsi->totalSize;
 	while (((long long)(scaledSz / 1024)) > 0) {
 		scaledSz /= 1024;
 		currUnit++;
 	}
-	printf("Size: %llu bytes (%.3Lf %s)\n", fat_fs->totalSize, scaledSz, units[currUnit]);
+	printf("Size: %llu bytes (%.3Lf %s)\n", fsi->totalSize, scaledSz, units[currUnit]);
 	printf("\n");
 	printf("Disk geometry:\n--------------\n");
 	printf("Bytes Per Sector: %d\n", bs->BPB_BytsPerSec);
 	printf("Sectors Per Cluster: %d\n", bs->BPB_SecPerClus);
-	printf("Total Sectors: %d\n", fat_fs->numSectors);
+	printf("Total Sectors: %d\n", fsi->numSectors);
 	printf("Physical - Sectors per Track: %d\n", bs->BPB_SecPerTrk);
 	printf("Physical - Heads: %d\n", bs->BPB_NumHeads);
 	printf("\n");
 	printf("File system info:\n-----------------\n");
 	printf("Volume ID: ");
-	if (fat_fs->type == FS_FAT32)
-		printf("%d", fat_fs->bootsect32->BS_VolID);
+	if (fsi->type == FS_FAT32)
+		printf("%d", fsi->bootsect32->BS_VolID);
 	else
-		printf("%d", fat_fs->bootsect16->BS_VolID);
+		printf("%d", fsi->bootsect16->BS_VolID);
 	printf("\n");
-	printf("File System Type (computed): %s\n", typeNames[fat_fs->type]);
-	printf("FAT Size (sectors): %d\n", fat_fs->FATsz);
+	printf("File System Type (computed): %s\n", typeNames[fsi->type]);
+	printf("FAT Size (sectors): %d\n", fsi->FATsz);
 	// read the FAT to figure out how much free space there is
 	printf("Free space: %d bytes\n", 0);
 	printf("\n");
 }
 
-void print_dir(FS_Instance * fat_fs, FS_CurrentDir current_dir) {
+void print_dir(FS_Instance * fsi, FS_CurrentDir current_dir) {
 
 }
 
-FS_CurrentDir change_dir(FS_Instance * fat_fs, FS_CurrentDir current_dir, char * path) {
+FS_CurrentDir change_dir(FS_Instance * fsi, FS_CurrentDir current_dir, char * path) {
 
 }
 
-void get_file(FS_Instance * fat_fs, FS_CurrentDir current_dir, char * path, char * local_path) {
+void get_file(FS_Instance * fsi, FS_CurrentDir current_dir, char * path, char * local_path) {
 
 }
 
-void put_file(FS_Instance * fat_fs, FS_CurrentDir current_dir, char * path, char * local_path) {
+void put_file(FS_Instance * fsi, FS_CurrentDir current_dir, char * path, char * local_path) {
 
 }
 
-void make_dir(FS_Instance * fat_fs, FS_CurrentDir current_dir, char * path) {
+void make_dir(FS_Instance * fsi, FS_CurrentDir current_dir, char * path) {
 
 }
 
-void delete_file(FS_Instance * fat_fs, FS_CurrentDir current_dir, char * path) {
+void delete_file(FS_Instance * fsi, FS_CurrentDir current_dir, char * path) {
 
 }
 
-void fs_cleanup(FS_Instance * fat_fs) {
-	if (NULL != fat_fs) {
-		if (NULL != fat_fs->disk) {
-			fclose(fat_fs->disk);
+void fs_cleanup(FS_Instance * fsi) {
+	if (NULL != fsi) {
+		if (NULL != fsi->disk) {
+			fclose(fsi->disk);
 		}
-		if (NULL != fat_fs->bootsect) {
-			free(fat_fs->bootsect);
+		if (NULL != fsi->bootsect) {
+			free(fsi->bootsect);
 		}
-		if (NULL != fat_fs->bootsect16) {
-			free(fat_fs->bootsect16);
+		if (NULL != fsi->bootsect16) {
+			free(fsi->bootsect16);
 		}
-		if (NULL != fat_fs->bootsect32) {
-			free(fat_fs->bootsect32);
+		if (NULL != fsi->bootsect32) {
+			free(fsi->bootsect32);
 		}
-		free(fat_fs);
+		free(fsi);
 	}
 }
--- fat_fs.h
+++ fat_fs.h
@@ -31,18 +31,18 @@ typedef struct FS_Instance_struct FS_Instance;
 typedef uint32_t FS_CurrentDir;
 
 FS_Instance * fs_create_instance(char * image_path);
-FS_CurrentDir fs_get_root(FS_Instance * fat_fs);
+FS_CurrentDir fs_get_root(FS_Instance * fsi);
 
-void print_info(FS_Instance * fat_fs);
-void print_dir(FS_Instance * fat_fs, FS_CurrentDir current_dir);
-FS_CurrentDir change_dir(FS_Instance * fat_fs, FS_CurrentDir current_dir, char * path);
-void get_file(FS_Instance * fat_fs, FS_CurrentDir current_dir, char * path, char * local_path);
+void print_info(FS_Instance * fsi);
+void print_dir(FS_Instance * fsi, FS_CurrentDir current_dir);
+FS_CurrentDir change_dir(FS_Instance * fsi, FS_CurrentDir current_dir, char * path);
+void get_file(FS_Instance * fsi, FS_CurrentDir current_dir, char * path, char * local_path);
 
 // These are for the bonus questions
-void put_file(FS_Instance * fat_fs, FS_CurrentDir current_dir, char * path, char * local_path);
-void make_dir(FS_Instance * fat_fs, FS_CurrentDir current_dir, char * path);
-void delete_file(FS_Instance * fat_fs, FS_CurrentDir current_dir, char * path);
+void put_file(FS_Instance * fsi, FS_CurrentDir current_dir, char * path, char * local_path);
+void make_dir(FS_Instance * fsi, FS_CurrentDir current_dir, char * path);
+void delete_file(FS_Instance * fsi, FS_CurrentDir current_dir, char * path);
 
-void fs_cleanup(FS_Instance *fat_fs);
+void fs_cleanup(FS_Instance * fsi);
 
 #endif
